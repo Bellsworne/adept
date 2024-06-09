7cd7ab5b84278c379670ac240c8129bc3614242b
Spudnut2000 <chrisbell@hexstudios.co>
2024-06-08 23:34:26 -0500

    Nodes and Scenes

--- ADEPT-Engine/Program.cs
+++ ADEPT-Engine/Program.cs
@@ -1,7 +1,108 @@
-﻿class Program
+﻿using System.Security.Cryptography;
+using ADEPT_Engine;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+using ImGuiNET;
+using rlImGui_cs;
+
+class Program
 {
+    private static int _screenWidth = 900;
+    private static int _screenHeight = 900;
+    private static string _windowTitle = "ADEPT";
+
+    private static string _treeText = "";
+
+    private static Scene _scene;
+    
     static void Main()
     {
-        Console.WriteLine("Test");
+        SetConfigFlags(ConfigFlags.Msaa4xHint | ConfigFlags.VSyncHint | ConfigFlags.ResizableWindow);
+        InitWindow(_screenWidth, _screenHeight, _windowTitle);
+        SetTargetFPS(60);
+
+        _scene = new();
+
+        Node node1 = new() { Name = "Node1" };
+        Node node2 = new() { Name = "Node2" };
+        Node node3 = new() { Name = "Node3" };
+        Node node4 = new() { Name = "Node4" };
+        Node node5 = new() { Name = "Node5" };
+        Node node6 = new() { Name = "Node6" };
+        Node node7 = new() { Name = "Node7" };
+        
+        node1.AddChild(node2);
+        node2.AddChild(node3);
+        _scene.Root.AddChild(node4);
+        node4.AddChild(node5);
+        node5.AddChild(node6);
+        _scene.Root.AddChild(node7);
+        
+        _scene.Root.AddChild(node1);
+        
+        rlImGui.Setup(true);
+        while (!WindowShouldClose())
+        {
+            if (IsWindowResized())
+            {
+                _screenHeight = GetScreenHeight();
+                _screenWidth = GetScreenWidth();
+            }
+            
+            _treeText = "";
+            UpdateTreeText(_scene.Root);
+            
+            Update();
+            Draw();
+        }
+        
+        Cleanup();
+    }
+    
+    
+    
+    static void UpdateTreeText(Node node, string indentation = "")
+    {
+        _treeText += indentation + node.Name + "\n";
+
+        foreach (var child in node.Children)
+        {
+            UpdateTreeText(child, indentation + "- ");
+        }
+    }
+
+    static void Update()
+    {
+        _scene.Update();
+
+        if (IsKeyPressed(KeyboardKey.K))
+        {
+            if (_scene.Root.GetChild("Node1") is { } node)
+            {
+                _scene.Root.RemoveChild(node);    
+            }
+        }
+    }
+
+    static void Draw()
+    {
+        BeginDrawing();
+        ClearBackground(Color.DarkGray);
+        
+        rlImGui.Begin();
+        if (ImGui.Begin("Debug window"))
+        {
+            ImGui.TextUnformatted(GetFPS().ToString());
+            ImGui.TextWrapped(_treeText);
+        }
+        rlImGui.End();
+        
+        EndDrawing();
+    }
+
+    static void Cleanup()
+    {
+        rlImGui.Shutdown();
+        CloseWindow();
     }
 }
--- ADEPT-Engine/src/Node.cs
+++ ADEPT-Engine/src/Node.cs
@@ -3,12 +3,69 @@ namespace ADEPT_Engine;
 public class Node
 {
     public string Name = String.Empty;
-
-
-    private List<Node> _children;
     
+    public List<Node> Children => _children;
+
+
+    private Node? _parent;
+    private List<Node> _children = new();
+
+
     public Node(){}
     
     public virtual void Start(){}
-    public virtual void Update(){}
+    
+    public virtual void Update()
+    {
+        foreach(var child in _children)
+        {
+            child.Update();
+        }
+    }
+
+    public Node? GetParent()
+    {
+        return _parent;
+    }
+
+    public void SetParent(Node newParent)
+    {
+        _parent?.RemoveChild(this);
+    }
+
+    public void AddChild(Node child)
+    {
+        if (_children.Contains(child)) return;
+        _children.Add(child);
+        child._parent = this;
+    }
+
+    public void RemoveChild(Node child)
+    {
+        if (_children.Contains(child))
+        {
+            _children.Remove(child);
+            child._parent = null;
+        }
+    }
+
+    public Node? GetChild(string name)
+    {
+        return _children.FirstOrDefault(child => child.Name == name);
+    }
+
+    public void Destroy()
+    {
+        if (_parent is not null)
+        {
+            _parent.RemoveChild(this);
+        }
+
+        foreach (var child in _children.ToList())
+        {
+            child.Destroy();
+        }
+
+        _children.Clear();
+    }
 }
